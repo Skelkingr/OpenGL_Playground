eb4b5087fcb73f63dda99118f55d34ad5e415238
Skelkingr <aymeric.goupil@gmail.com>
2022-12-25 12:41:15 +0100

    Remove text shaders from Util.h : no longer of use.

--- util.h
+++ util.h
@@ -1,8 +1,6 @@
 #ifndef UTIL_H
 #define UTIL_H
 
-#define STB_IMAGE_IMPLEMENTATION 
-
 // INCLUDES
 #include <cmath>
 #include <iostream>
@@ -19,41 +17,8 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include "stb_image.h"
-
 // CONSTANTS
 const float TO_RADIANS = 3.14159265f / 180.0f;
 
-// TEXT SHADERS
-static const char* VSHADER = "									\n\
-#version 330													\n\
-																\n\
-layout (location = 0) in vec3 pos;								\n\
-layout (location = 1) in vec3 vertexColors;						\n\
-																\n\
-out vec3 v_vertexColors;										\n\
-																\n\
-uniform mat4 model;												\n\
-uniform mat4 projection;										\n\
-																\n\
-void main()														\n\
-{																\n\
-	gl_Position = projection * model * vec4(pos, 1.0);			\n\
-	v_vertexColors = vertexColors;								\n\
-}																\n\
-";
-
-static const char* FSHADER = "													\n\
-#version 330																	\n\
-																				\n\
-in vec3 v_vertexColors;															\n\
-																				\n\
-out vec4 color;																	\n\
-																				\n\
-void main()																		\n\
-{																				\n\
-	color = vec4(v_vertexColors.r, v_vertexColors.g, v_vertexColors.b, 1.0);	\n\
-}																				\n\
-";
-
 #endif
+
