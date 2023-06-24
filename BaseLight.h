0605e1d4ea45a7a6e0a69eb397a10eb8b7e7dc6d
Skelkingr <aymeric.goupil@gmail.com>
2023-06-24 21:24:46 +0200

    Update BaseLight header and source.

--- BaseLight.h
+++ BaseLight.h
@@ -8,13 +8,12 @@ class BaseLight
 {
 public:
 	BaseLight();
-	BaseLight(GLuint shadowWidth, GLuint shadowHeight, glm::vec3 colour, GLfloat ambientIntensity, GLfloat diffuseIntensity);
+	BaseLight(GLfloat shadowWidth, GLfloat shadowHeight, glm::vec3 colour, GLfloat ambientIntensity, GLfloat diffuseIntensity);
 	~BaseLight();
 
 	glm::vec3 GetColour() const { return mAmbientColour; }
 	GLfloat GetAmbientIntensity() const { return mAmbientIntensity; }
 	GLfloat GetDiffuseIntensity() const { return mDiffuseIntensity; }
-
 	ShadowMap* GetShadowMap() const { return mShadowMap; }
 
 protected:
