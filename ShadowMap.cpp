7ad4bf8714068545fb0454211b54953f43e5380c
Skelkingr <aymeric.goupil@gmail.com>
2023-06-25 20:23:42 +0200

    Update ShadowMap source.

--- ShadowMap.cpp
+++ ShadowMap.cpp
@@ -32,6 +32,8 @@ GLboolean ShadowMap::Init(GLuint width, GLuint height)
 	glBindTexture(GL_TEXTURE_2D, mShadowMap);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, mShadowWidth, mShadowHeight, 0, GL_DEPTH_COMPONENT, GL_FLOAT, nullptr);
 	
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
 	
@@ -47,7 +49,6 @@ GLboolean ShadowMap::Init(GLuint width, GLuint height)
 	glReadBuffer(GL_NONE);
 
 	GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
-
 	if (status != GL_FRAMEBUFFER_COMPLETE)
 	{
 		printf("[ERR] Framebuffer error: %i\n", status);
